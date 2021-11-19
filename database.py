64c2c9e81ec58c038489258eab547614f4abeaa9
ilhamalvindo <ilhamalvindo@gmail.com>
2021-11-19 20:47:19 +0700

    change database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgres://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
+SQLALCHEMY_DATABASE_URL = "postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
