a01d603f63364cfff3f8ae7135641fd388469cf6
ilhamalvindo <ilhamalvindo@gmail.com>
2021-11-19 20:52:08 +0700

    solve database connect problem

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
 
+#"postgresql://postgres:vestionarmy73@localhost:5432/local_db_tst_rpl" //local database
+#"postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
