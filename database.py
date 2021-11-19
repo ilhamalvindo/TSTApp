7ace4101fd8f7d664dcab3c904beae8a4c7dafb5
ilhamalvindo <ilhamalvindo@gmail.com>
2021-11-19 20:43:12 +0700

    change sqlalchemy database url to xpjfchbo user

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:vestionarmy73@localhost:5432/local_db_tst_rpl"
+SQLALCHEMY_DATABASE_URL = "postgres://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
